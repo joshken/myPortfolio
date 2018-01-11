bd8643071f509b3d425d9ce1d33f08d551ea6014
Josh Kennelly <joshkennelly@Joshs-iMac.local>
2018-01-11 12:02:17 -1000

    New Design

---
+++ js/contentController.js
@@ -0,0 +1,86 @@
+var currentLayout = 3;
+var resizeOccured = false;
+
+function refreshLayout() {
+    var cellWidth = $(".grid div").eq(0).width();
+    var containerWidth = $(".grid").width();
+
+    if (cellWidth/containerWidth < 0.4) {
+        currentLayout = 3;
+    } else {
+        currentLayout = 2;
+    }
+}
+
+$(document).ready(function() {
+    
+    refreshLayout();
+    
+    // Add toggle events for navigations buttons
+    $("nav button").click(function() {
+        // Toggle the button
+        if ($(this).hasClass("selected")) {
+            // Do nothing
+        } else {
+            $("nav .selected").removeClass("selected");
+            $(this).addClass("selected");
+            
+            // Load content
+            var selectedContent = $("nav .selected").html();
+            if (selectedContent == "Graphic Design") {
+                $("section").html(getGraphics()).removeClass("grid");
+            } else {
+                location.href = "index.html";
+            }
+        }
+    });
+    
+    // Add click events for images in the grid
+    $(".grid div img").click(function() {
+        $img = $(this);
+        
+        // Unselect all other images and close the summary box
+        $(".grid div img").not(this).removeClass("selected");
+        $(".grid div span").remove();
+        $("summary").remove();
+        
+        // If the screen was just resized we don't want to close the summary box
+        if (resizeOccured) {
+            $img.removeClass("selected");
+        }
+        
+        // If the current image was clicked close the summary box; otherwise, open the new one
+        if ($img.hasClass("selected")) {
+            $img.removeClass("selected");
+        } else {
+            // Find out where to insert the summary box
+            var index = $img.parent().index();
+            var $insertPoint = $(".grid div").eq(sumPosition[currentLayout][index]);
+            
+            // Insert the box in the correct position
+            $("<summary></summary>").insertAfter($insertPoint);
+            
+            // Fill in the summary
+            $("summary").html(getSummary(index));
+            
+            // Open the summary box and select the image
+            $("summary").addClass("slide").after(function() {
+                $img.parent().append("<span class='show'></span>");
+                $img.addClass("selected");
+            });
+        }
+    });
+});
+
+$(window).resize(function() {
+    
+    var oldLayout = currentLayout;
+    
+    refreshLayout();
+    
+    if (oldLayout != currentLayout) {
+        resizeOccured = true;
+        $(".grid div .selected").click();
+        resizeOccured = false;
+    }
+});
---
+++ js/graphicDesign.js
@@ -0,0 +1,97 @@
+var graphics = [];
+graphics[0] = {
+    image: "graphicDesign/ACCTG205/ACCTG205_LgBanner.png",
+    template: "graphicDesign/ACCTG205/"
+};
+graphics[1] = {
+    image: "graphicDesign/AGBUS105/AG105LgBanner.jpg",
+    template: "graphicDesign/AGBUS105/"
+};
+graphics[2] = {
+    image: "graphicDesign/ART160/Images/largeBanner.jpg",
+    template: "graphicDesign/ART160/"
+};
+graphics[3] = {
+    image: "graphicDesign/B283/B283LgBanner.png",
+    template: "graphicDesign/B283/"
+};
+graphics[4] = {
+    image: "graphicDesign/B341/B341LgBanner.jpg",
+    template: "graphicDesign/B341/"
+};
+graphics[5] = {
+    image: "graphicDesign/CIT241/CIT241_LgBanner.png",
+    template: "graphicDesign/CIT241/"
+};
+graphics[6] = {
+    image: "graphicDesign/COMM307/Comm307LgBanner.png",
+    template: "graphicDesign/COMM307/"
+};
+graphics[7] = {
+    image: "graphicDesign/CS246/CS246_LgBanner_01.jpg",
+    template: "graphicDesign/CS246/"
+};
+graphics[8] = {
+    image: "graphicDesign/ECEN150/Images/largeBanner.jpg",
+    template: "graphicDesign/ECEN150/"
+};
+graphics[9] = {
+    image: "graphicDesign/ECON150/ECON150LgBanner.jpg",
+    template: "graphicDesign/ECON150/"
+};
+graphics[10] = {
+    image: "graphicDesign/ED313/ED313LgBanner.png",
+    template: "graphicDesign/ED313/"
+};
+graphics[11] = {
+    image: "graphicDesign/ESS375/ESS375_LgBanner_01.JPG",
+    template: "graphicDesign/ESS375/"
+};
+graphics[12] = {
+    image: "graphicDesign/FDAMF101/FdFreedomLgBanner.jpg",
+    template: "graphicDesign/FDAMF101/"
+};
+graphics[13] = {
+    image: "graphicDesign/HFED340/HFED340LgBanner.png",
+    template: "graphicDesign/HFED340/"
+};
+graphics[14] = {
+    image: "graphicDesign/MATH100B/largeBanner.jpg",
+    template: "graphicDesign/MATH100B/"
+};
+graphics[15] = {
+    image: "graphicDesign/PrePathwayEnglish/PrePthwyLgBanner.jpg",
+    template: "graphicDesign/PrePathwayEnglish/"
+};
+graphics[16] = {
+    image: "graphicDesign/RM373/RM373_LgBanner_01.JPG",
+    template: "graphicDesign/RM373/"
+};
+graphics[17] = {
+    image: "graphicDesign/SOC323/largeBanner.jpg",
+    template: "graphicDesign/SOC323/"
+};
+graphics[18] = {
+    image: "graphicDesign/SPAN101/largeBanner.jpg",
+    template: "graphicDesign/SPAN101/"
+};
+graphics[19] = {
+    image: "graphicDesign/SPAN102/largeBanner.jpg",
+    template: "graphicDesign/SPAN102/"
+};
+graphics[20] = {
+    image: "graphicDesign/SPAN302/largeBanner.jpg",
+    template: "graphicDesign/SPAN302/"
+};
+
+function getGraphics() {
+    var graphicsList = "";
+    for (var i = 0; i < graphics.length; i++) {
+        graphicsList += '<div onclick="location.href=\'';
+        graphicsList += graphics[i].template;
+        graphicsList += '\'"><img src="';
+        graphicsList += graphics[i].image;
+        graphicsList += '" /></div>';
+    }
+    return graphicsList;
+}
